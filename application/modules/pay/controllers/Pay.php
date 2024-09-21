4691b3fbd911f0c8c3e8385bd4f2dca689ff3eaf
keatonmurray <murraykeaton422@gmail.com>
2024-09-21 00:38:27 -0400

    removed unused libraries

--- application/modules/pay/controllers/Pay.php
+++ application/modules/pay/controllers/Pay.php
@@ -1,90 +1,89 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-use Rinvex\Country\CountryLoader;
+    defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Pay extends MX_Controller {
+    class Pay extends MX_Controller {
 
-    public function __construct()
-    {
-        parent::__construct();
-        $this->load->helper('url');
-        $this->load->library('form_validation');
-        $this->load->model('Pay_model');
+        public function __construct()
+        {
+            parent::__construct();
+            $this->load->helper('url');
+            $this->load->library('form_validation');
+            $this->load->model('Pay_model');
+        }
+
+        public function index()
+        {
+            $data['layout'] = $this->load->view('head', NULL, TRUE);
+            $data['footer'] = $this->load->view('footer', NULL, TRUE);
+            $this->load->view('index', $data);
+        }
+
+        public function store()
+        {
+            $validateForm = [
+                [
+                    'field' => 'first_name',
+                    'label' => 'first name',
+                    'rules' => 'required'
+                ],
+                [
+                    'field' => 'last_name',
+                    'label' => 'last name',
+                    'rules' => 'required'
+                ],
+                [
+                    'field' => 'street_name',
+                    'label' => 'street name',
+                    'rules' => 'required'
+                ],
+                [
+                    'field' => 'city',
+                    'label' => 'city',
+                    'rules' => 'required'
+                ],
+                [
+                    'field' => 'state',
+                    'label' => 'state',
+                    'rules' => 'required'
+                ],
+                [
+                    'field' => 'zip_code',
+                    'label' => 'zip code',
+                    'rules' => 'required'
+                ],
+                [
+                    'field' => 'country',
+                    'label' => 'country',
+                    'rules' => 'required'
+                ]
+            ];
+
+            $this->form_validation->set_rules($validateForm);
+
+            if ($this->form_validation->run() === TRUE) {
+                $this->make_payment();
+            } 
+        }
+
+        private function insert_form_data()
+        {
+            $data = array(
+                'first_name' => $this->input->post('first_name'),
+                'last_name' => $this->input->post('last_name'),
+                'street_name' => $this->input->post('street_name'),
+                'city' => $this->input->post('city'),
+                'state' => $this->input->post('state'),
+                'zip_code' => $this->input->post('zip_code'),
+                'country' => $this->input->post('country')
+            );
+
+            $this->Pay_model->insert($data);
+        }
+
+        private function make_payment()
+        {
+            $success = $this->insert_form_data();
+            $amount = $this->input->post('amount');
+            echo Modules::run('paypal/init_payment', $amount, $success);
+        }
     }
-
-    public function index()
-    {
-		$data['layout'] = $this->load->view('head', NULL, TRUE);
-        $data['footer'] = $this->load->view('footer', NULL, TRUE);
-        $this->load->view('index', $data);
-    }
-
-    public function store()
-    {
-        $validateForm = [
-            [
-                'field' => 'first_name',
-                'label' => 'first name',
-                'rules' => 'required'
-            ],
-            [
-                'field' => 'last_name',
-                'label' => 'last name',
-                'rules' => 'required'
-            ],
-            [
-                'field' => 'street_name',
-                'label' => 'street name',
-                'rules' => 'required'
-            ],
-            [
-                'field' => 'city',
-                'label' => 'city',
-                'rules' => 'required'
-            ],
-            [
-                'field' => 'state',
-                'label' => 'state',
-                'rules' => 'required'
-            ],
-            [
-                'field' => 'zip_code',
-                'label' => 'zip code',
-                'rules' => 'required'
-            ],
-            [
-                'field' => 'country',
-                'label' => 'country',
-                'rules' => 'required'
-            ]
-        ];
-
-        $this->form_validation->set_rules($validateForm);
-
-        if ($this->form_validation->run() === TRUE) {
-            $this->make_payment();
-        } 
-    }
-
-    private function insert_form_data()
-    {
-        $data = array(
-            'first_name' => $this->input->post('first_name'),
-            'last_name' => $this->input->post('last_name'),
-            'street_name' => $this->input->post('street_name'),
-            'city' => $this->input->post('city'),
-            'state' => $this->input->post('state'),
-            'zip_code' => $this->input->post('zip_code'),
-            'country' => $this->input->post('country')
-        );
-
-        $this->Pay_model->insert($data);
-    }
-
-    private function make_payment()
-    {
-        $success = $this->insert_form_data();
-        $amount = $this->input->post('amount');
-        echo Modules::run('paypal/init_payment', $amount, $success);
-    }
-}
--- application/views/head.php
+++ application/views/head.php
@@ -12,7 +12,6 @@
         <link rel="stylesheet" href="<?php echo base_url('assets/css/global.css'); ?>">
         <script src="https://code.jquery.com/jquery-3.6.0.min.js" defer></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous" defer></script>
-        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11" defer></script>
         <script src="<?php echo base_url('assets/js/jquery.validate.min.js'); ?>" defer></script>
         <script src="<?php echo base_url('assets/js/script.js'); ?>" defer></script>
     </head>
